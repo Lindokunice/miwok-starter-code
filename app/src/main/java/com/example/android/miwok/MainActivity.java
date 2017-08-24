6035422b67f960dc60cd15be235b2d3ad8861bee
Lindokunice <lindokuhlemathibela@gmail.com>
2017-08-24 10:51:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
+        setContentView(R.layout.activity_main);
+
+        // Find the View that shows the numbers category
+        TextView numbers = (TextView) findViewById(R.id.NumbersActivity);
+
+        // Set a click listener on that View
+        numbers.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link NumbersActivity}
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+
+                // Start the new activity
+                startActivity(numbersIntent);
+            }
+        });
+
+        // Find the View that shows the food category
+        TextView Family = (TextView) findViewById(R.id.Food);
+
+        // Set a click listener on that View
+        food.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the family category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link FoodActivity}
+                Intent foodIntent = new Intent(MainActivity.this, FoodActivity.class);
+
+                // Start the new activity
+                startActivity(foodIntent);
+            }
+        });
+
+        // Find the View that shows the art_and_culture category
+        TextView Colors = (TextView) findViewById(R.id.Art);
+
+        // Set a click listener on that View
+        art_and_culture_culture.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the colors category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link ArtAndCultureActivity}
+                Intent art_and_cultureIntent = new Intent(MainActivity.this, ArtAndCultureActivity.class);
+
+                // Start the new activity
+                startActivity(art_and_cultureIntent);
+            }
+        });
+
+        // Find the View that shows the shopping centre category
+        TextView Phrases = (TextView) findViewById(R.id.Shopping);
+
+        // Set a click listener on that View
+        shopping_centre.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the phrases category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the {@link ShoppingCentreActivity}
+                Intent clothingIntent = new Intent(MainActivity.this, ShoppingCentreActivity.class);
+
+                // Start the new activity
+                startActivity(clothingIntent);
+            }
+        });
+    }
+}
